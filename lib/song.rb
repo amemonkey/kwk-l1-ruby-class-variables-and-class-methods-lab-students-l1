5401c8d75ecf489f993632f141e992d596c94eda
amemonkey <ameliafunaki789@gmail.com>
2018-06-11 15:43:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,15 @@
+class Song
+  @@count = 0
+  
+  def intialize
+    @@count => 1
+  end
+  
+  def self.count
+# Song.count
+# Song.artists
+# Song.genres
+# Song.genre_count
+# Song.artist_count
+
 
